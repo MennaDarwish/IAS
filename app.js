b943c654971ac5f3269aa7f764435a713addfa0c
MohamedMarwan <mohamed_marwan_m@hotmail.com>
2015-03-24 18:29:58 +0200

    UserActions and ActionTypes Routes added to app.js. #34

--- app.js
+++ app.js
@@ -11,5 +11,7 @@ var adRequest = require('./routes/adRequest');
 app.use('/adRequest',adRequest);
 app.use('/users', usersRoute);
 app.use('/publishers', publishersRoute);
+app.use('/actiontypes', actionTypesRoute);
+app.use('/useractions', userActionsRoute);
 
 module.exports = app;
