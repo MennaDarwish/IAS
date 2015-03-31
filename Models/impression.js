f4dc41613a32c74dfbc29d9864264d3aeb9ccfe4
MennaDarwish <menna.darwish1@gmail.com>
2015-03-31 20:15:44 +0200

    Creating a model Ad #41

---
+++ Models/impression.js
@@ -0,0 +1,15 @@
+"use strict" ; 
+module.exports = function(sequelize, DataTypes) {
+  var Impression = sequelize.define("Impression", {
+    // impression attributes 
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Impression.belongsTo(models.User, {foreignKey: 'userId'});
+        Impression.belongsTo(model.Ad , {forgein: 'adID'});
+
+      }
+    }
+  });
+  return Impression;
+};
