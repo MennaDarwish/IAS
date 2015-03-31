d99a31a215baf42b6245e62867cdeab3b36b08d5
MennaDarwish <menna.darwish1@gmail.com>
2015-03-31 20:22:37 +0200

    Fixed the code according to Models convention

--- Models/impression.js
+++ Models/impression.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Impression.belongsTo(models.User, {foreignKey: 'userId'});
-        Impression.belongsTo(model.Ad , {forgein: 'adID'});
+        Impression.belongsTo(model.Ad , {forgein: 'adId'});
 
       }
     }
