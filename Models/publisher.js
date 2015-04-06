1c3327141bdcaa113de11a9c7ce62d25010da392
3adel94 <3adel94@gmail.com>
2015-04-06 19:21:25 +0200

    Added authentication for Publisher

--- Models/publisher.js
+++ Models/publisher.js
@@ -2,6 +2,8 @@
 module.exports = function(sequelize, DataTypes) {
   var Publisher = sequelize.define("Publisher", {
     name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
     domain: DataTypes.STRING,
     email: DataTypes.STRING,
     channel: DataTypes.STRING
