499c64fb8484bf372b8152bfcdc6654152e0f14d
3adel94 <3adel94@gmail.com>
2015-04-09 00:16:07 +0200

    added authentication for publisher

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var jade = require('jade');
-var app = express();
-//Routes
-var usersRoute = require('./routes/users');
-var publishersRoute = require('./routes/publishers');
-var actionTypesRoute = require('./routes/actionTypes');
-var userActionsRoute = require('./routes/userActions');
-var adRequest = require('./routes/adRequest');
-var querystring = require('querystring');
-
-var auth = require('./auth.js');
+var passport = require('passport');
 var morgan = require('morgan');
 
-app.use(auth.initialize());
+
+
+var app = express();
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
+
+
 app.use(express.static(__dirname + '/public'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+var usersRoute = require('./routes/users');
+var publishersRoute = require('./routes/publishers');
+var express = require('express');
+var querystring = require('querystring');
+var adRequest = require('./routes/adRequest');
+
+
+
 app.use('/adRequest',adRequest);
 app.use('/users', usersRoute);
 app.use('/publishers', publishersRoute);
-app.use('/actiontypes', actionTypesRoute);
-app.use('/useractions', userActionsRoute);
-
-app.listen(3000);
-
 
 module.exports = app;
--- routes/publishers.js
+++ routes/publishers.js
@@ -1,30 +1,49 @@
 var express  = require('express');
 var bodyParser = require('body-parser');
 var urlEncoded = bodyParser.urlencoded({ extended: false });
-var uuid = require('node-uuid');
-var auth = require('../auth.js');
 var router = express.Router();
-var id = uuid.v4();
-var secret = uuid.v4();
+var Publisher = require('../models/index.js').Publisher;
+var publisherAuthentication = require('../lib/publisherAuthentication.js');
 
-var Publisher = require('../Models/index.js').Publisher;
+
+passport.use('local-signup', new passportLocal(
+  {passReqToCallback : true}, 
+ function(req, username, password, done) {
+    publisherAuthentication.localReg(username, password)
+    
+    .then(function (user) {
+      if (user) {
+        console.log("LOGGED IN AS: " + user.username);
+        req.session.success = 'You are successfully logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT LOG IN");
+        req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+   }
+    
+  ));
 var publisherBuilder = function(req, res, next) {
   var publisher = {
     name: req.body.name,
     domain: req.body.domain,
     email: req.body.email,
-    channel: req.body.channel,
-    apikey: id + ':' + secret
+    channel: req.body.channel
   }
   req.body.publisher = publisher;
   next();
 }
-
 router.route('/')
   .post(urlEncoded, publisherBuilder, function(req, res) {
     var publisher = req.body.publisher;
     Publisher.create(publisher).then(function(createdPublisher) {
-      res.status(201).json({status: 'created', publisherId: createdPublisher.dataValues.id, apikey: createdPublisher.dataValues.apikey});
+      res.status(201).json({status: 'created', publisherId: createdPublisher.dataValues.id});
     }, function(err) {
       res.status(404).json({status: 'ERROR', message: 'Something went wrong ' + err});
     });
@@ -35,5 +54,7 @@ router.route('/sign_up')
       title : 'Publisher Sign Up'
     })
   });
+ router.route('/sign_up')
+  .post(passport.authenticate('local-signup'));
 
 module.exports = router;
