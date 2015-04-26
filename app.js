ce1e5ddde3f9ec60d65cedf78ca24a5b8a60e3ed
MennaDarwish <menna.darwish1@gmail.com>
2015-04-27 00:25:39 +0300

    Added a route for publisher signup, created a a basic view in (homepage.ejs) , added a route for publisher profile, created a blank view for publishersProfile, added a function to serialize/deserialize the publisher from session #62

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var jade = require('jade');
 var app = express();
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 //Routes
 var usersRoute = require('./routes/users');
 var publishersRoute = require('./routes/publishers');
@@ -9,12 +13,22 @@ var userActionsRoute = require('./routes/userActions');
 var adRequest = require('./routes/adRequest');
 var querystring = require('querystring');
 
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(session({
+secret: process.env.SESSION_SECRET || 'secret',
+resave: false,
+saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 var auth = require('./auth.js');
 var morgan = require('morgan');
 
 app.use(auth.initialize());
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use('/adRequest',adRequest);
